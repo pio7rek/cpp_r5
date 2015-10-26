0be6089cd4bd892ea86949d91f2b9a272cf2bd1f
Piotrek <pio7rek@gmail.com>
2015-10-26 13:08:58 +0100

    rozdział 5.6.4

--- cpp_r5/src/cpp_r5.cpp
+++ cpp_r5/src/cpp_r5.cpp
@@ -8,8 +8,28 @@
 
 #include <iostream>
 using namespace std;
-
+void error(const string s) {
+	throw runtime_error(s);
+}
 int main() {
+	try {
+		double d;
+		cin >> d;
+		if (!cin) {
+			error("cos posz³o nie tak z wczytaniem liczby!\n");
+		}
+	}
+/*	catch (runtime_error& e) {
+		cerr << "runtime_error: " << e.what() << '\n';
+		return 1;
+	}
+	catch (...) {
+		cerr << "nieznany b³¹d\n";
+		return 2;
+	}*/
+	catch (out_of_range& e) {
+		return 3;
+	}
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
